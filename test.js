375600a8be49e3c09998e5050d464de140eaf562
kuuuube <hexagonisalie@gmail.com>
2025-08-29 13:21:49 -0400

    Cleanup

--- test.js
+++ test.js
@@ -1,37 +1,50 @@
-const radicals_selection = document.querySelector("#radicals-selection");
 let selected_radicals = [];
 
-let current_stroke_count = 1;
+function prepare_radicals_selection() {
+    const radicals_selection = document.querySelector("#radicals-selection");
 
-let radicals_selection_innerHTML_string = "";
-for (const [radical, data] of Object.entries(RADKFILEX)) {
-    if (data.stroke_count !== current_stroke_count) {
-        current_stroke_count = data.stroke_count;
-        console.log(radical);
-        if (data.stroke_count !== 0) {
-            radicals_selection_innerHTML_string += "</div>";
+    let current_stroke_count = 1;
+    let radicals_selection_innerHTML_string = "";
+    for (const [radical, data] of Object.entries(RADKFILEX)) {
+        if (data.stroke_count !== current_stroke_count) {
+            current_stroke_count = data.stroke_count;
+            console.log(radical);
+            if (data.stroke_count !== 0) {
+                radicals_selection_innerHTML_string += "</div>";
+            }
+            radicals_selection_innerHTML_string += "<div id=\"count-" + current_stroke_count + "\">";
         }
-        radicals_selection_innerHTML_string += "<div id=\"count-" + current_stroke_count + "\">";
+        radicals_selection_innerHTML_string += "<span>" + radical + "</span>";
     }
-    radicals_selection_innerHTML_string += "<span>" + radical + "</span>";
+    radicals_selection_innerHTML_string += "</div>"
+    radicals_selection.innerHTML = radicals_selection_innerHTML_string;
+
+    radicals_selection.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        const radical = e.target.textContent;
+        if (radical.length > 1) { return; }
+        if (selected_radicals.indexOf(radical) == -1) {
+            selected_radicals.push(radical);
+            e.target.style.color = "red";
+        } else {
+            selected_radicals.splice(selected_radicals.indexOf(radical), 1);
+            e.target.style.color = "";
+        }
+
+        find_possible_kanji();
+    });
 }
-radicals_selection_innerHTML_string += "</div>"
-radicals_selection.innerHTML = radicals_selection_innerHTML_string;
 
-radicals_selection.addEventListener("click", (e) => {
-    const radical = e.target.textContent;
-    if (radical.length > 1) { return; }
-    if (selected_radicals.indexOf(radical) == -1) {
-        selected_radicals.push(radical);
-        e.target.style.color = "red";
-    } else {
-        selected_radicals.splice(selected_radicals.indexOf(radical), 1);
-        e.target.style.color = "";
-    }
-
+function find_possible_kanji() {
     let possible_kanji = selected_radicals.length > 0 ? RADKFILEX[selected_radicals[0]]["kanji"] : [];
     for (let i = 1; i < selected_radicals.length; i++) {
         possible_kanji = possible_kanji.filter((x) => RADKFILEX[selected_radicals[i]]["kanji"].includes(x))
     }
-    document.querySelector("#kanji").innerHTML = possible_kanji.join("") + "&nbsp;";
-});
+
+    document.querySelector("#kanji").innerHTML = possible_kanji.length ? possible_kanji.join("") : "&nbsp;";
+}
+
+
+prepare_radicals_selection();
