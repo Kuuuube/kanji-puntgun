e0c58d89686bf639e32b0493b8b30a660c472935
kuuuube <hexagonisalie@gmail.com>
2025-08-29 22:46:02 -0400

    Move asset paths to variables

--- asset_builder.py
+++ asset_builder.py
@@ -2,18 +2,22 @@ import json
 import re
 import os
 
+assets_dir = "./assets/"
+data_assets_dir = assets_dir + "data/"
+generated_assets_dir = assets_dir + "generated/"
+static_assets_dir = assets_dir + "static_data/"
+
 def write_js_json(json_object, name):
-    output_dir = "./assets/generated/"
-    os.makedirs(output_dir, exist_ok = True)
+    os.makedirs(generated_assets_dir, exist_ok = True)
     json_string = json.dumps(json_object, ensure_ascii = False, indent = 4)
-    with open(output_dir + name + ".json", "w", encoding = "utf8") as components_json:
+    with open(generated_assets_dir + name + ".json", "w", encoding = "utf8") as components_json:
         components_json.write(json_string)
-    with open(output_dir + name + ".js", "w", encoding = "utf8") as components_js:
+    with open(generated_assets_dir + name + ".js", "w", encoding = "utf8") as components_js:
         components_js.write("const " + name.upper() + " = " + json_string)
 
 def generate_components_file():
     kanji_stroke_counts = generate_kanjidic_stroke_count()
-    radkfile_lines = list(map(str.strip, open("./assets/data/radkfilex", "r", encoding = "euc_jisx0213").readlines()))
+    radkfile_lines = list(map(str.strip, open(data_assets_dir + "radkfilex", "r", encoding = "euc_jisx0213").readlines()))
 
     header = ""
     components_dict = {}
@@ -33,7 +37,7 @@ def generate_components_file():
 def generate_kanjidic_stroke_count():
     kanji_stroke_counts = {}
 
-    kanjidic = open("./assets/data/kanjidic2.xml").read().replace("\n", "").replace("\r", "")
+    kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
     for character_data in re.findall("<character>.*?</character>", kanjidic):
         character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)
         stroke_count = re.search(r"(?<=<stroke_count>)\d+(?=</stroke_count>)", character_data)
@@ -53,7 +57,7 @@ def generate_four_corner_file():
         "extra": {"0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []},
     }
 
-    kanjidic = open("./assets/data/kanjidic2.xml").read().replace("\n", "").replace("\r", "")
+    kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
     for character_data in re.findall("<character>.*?</character>", kanjidic):
         character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)[0]
         four_corner_code = re.search(r"(?<=<q_code qc_type=\"four_corner\">).*?(?=</q_code>)", character_data)
@@ -67,15 +71,15 @@ def generate_four_corner_file():
 
     write_js_json(four_corner_codes_dict, "four_corner")
 
-    with open("./assets/generated/four_corner.js", "a") as four_corner_js_output:
+    with open(generated_assets_dir + "four_corner.js", "a") as four_corner_js_output:
         four_corner_js_output.write("\n")
         four_corner_js_output.write("const FOUR_CORNER_INFO = ")
-        four_corner_js_output.write(open("./assets/static_data/four_corner_info.json").read())
+        four_corner_js_output.write(open(static_assets_dir + "four_corner_info.json").read())
 
 def generate_radicals_file():
     radicals_dict = {}
 
-    radicals_info = json.loads(open("./assets/static_data/radicals_info.json").read())
+    radicals_info = json.loads(open(static_assets_dir + "radicals_info.json").read())
     for radical_info in radicals_info:
         radical_id = radical_info["radical_id"]
         if radical_id not in radicals_dict.keys():
@@ -89,7 +93,7 @@ def generate_radicals_file():
         else:
             radicals_dict[radical_info["radical_id"]]["radical_characters"].append({"character": radical_info["character"], "stroke_count": radical_info["stroke_count"]})
 
-    kanjidic = open("./assets/data/kanjidic2.xml").read().replace("\n", "").replace("\r", "")
+    kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
     for character_data in re.findall("<character>.*?</character>", kanjidic):
         character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)[0]
         radical = re.search(r"(?<=<rad_value rad_type=\"classical\">).*?(?=</rad_value>)", character_data)
