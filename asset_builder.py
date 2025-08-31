58d0180b39cec69aa3e339bd1531deb19fc2079e
kuuuube <hexagonisalie@gmail.com>
2025-08-31 12:50:07 -0400

    Change asset_builder to per kanji data and small helper file architecture

--- asset_builder.py
+++ asset_builder.py
@@ -2,6 +2,7 @@ import json
 import re
 import os
 import unicodedata
+import copy
 
 assets_dir = "./assets/"
 data_assets_dir = assets_dir + "data/"
@@ -23,124 +24,136 @@ def write_js_json(json_object, name):
     with open(generated_assets_dir + name + ".js", "w", encoding = "utf8") as components_js:
         components_js.write("const " + name.upper() + " = " + json_string)
 
-def generate_components_file():
-    kanji_stroke_counts = generate_kanjidic_stroke_count()
+kanji_stroke_counts = {}
+
+def generate_components_data():
     radkfile_lines = list(map(str.strip, open(data_assets_dir + "radkfilex", "r", encoding = "euc_jisx0213").readlines()))
 
     header = ""
     components_dict = {}
+    components_list = []
     for radkfile_line in radkfile_lines:
         if len(radkfile_line) <= 0 or radkfile_line[0] == "#":
             continue
         if radkfile_line[0] == "$":
             header = radkfile_line.split()[1]
-            components_dict[header] = {"kanji": [], "stroke_count": radkfile_line.split()[2]}
+            stroke_count = radkfile_line.split()[2]
+            components_dict[header] = {"kanji": [], "stroke_count": stroke_count}
+            components_list.append({"component": header, "stroke_count": stroke_count})
             continue
 
         components_dict[header]["kanji"] += list(radkfile_line)
         components_dict[header]["kanji"].sort(key = lambda x: kanji_stroke_counts[x])
 
-    write_js_json(components_dict, "components")
+    write_js_json(components_list, "components_list")
 
-def generate_kanjidic_stroke_count():
-    kanji_stroke_counts = {}
+    return components_dict
 
-    kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
-    for character_data in re.findall("<character>.*?</character>", kanjidic):
-        character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)
-        stroke_count = re.search(r"(?<=<stroke_count>)\d+(?=</stroke_count>)", character_data)
+def parse_kanjidic_data():
+    radicals_info_dict = {}
 
-        if character and stroke_count:
-            kanji_stroke_counts[character[0]] = int(stroke_count[0])
+    kanji_data = {}
 
-    write_js_json(kanji_stroke_counts, "kanji_stroke_counts")
-    return kanji_stroke_counts
-
-def generate_four_corner_file():
-    four_corner_codes_dict = {
-        "top_left": {"0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []},
-        "top_right": {"0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []},
-        "bottom_left": {"0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []},
-        "bottom_right": {"0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []},
-        "extra": {"0": [], "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": []},
+    default_kanji_data = {
+    "skip": {
+        "part_one": -1,
+        "part_two": -1,
+        "part_three": -1,
+    },
+    "four_corner": {
+        "top_left": -1,
+        "top_right": -1,
+        "bottom_left": -1,
+        "bottom_right": -1,
+        "extra": -1,
+    },
+        "radical": {
+            "id": -1,
+            "characters": [],
+        },
+        "components": [],
     }
 
-    kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
-    for character_data in re.findall("<character>.*?</character>", kanjidic):
-        character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)[0]
-        four_corner_code = re.search(r"(?<=<q_code qc_type=\"four_corner\">).*?(?=</q_code>)", character_data)
-        if four_corner_code:
-            four_corner_code_str = four_corner_code[0]
-            four_corner_codes_dict["top_left"][four_corner_code_str[0]].append(character)
-            four_corner_codes_dict["top_right"][four_corner_code_str[1]].append(character)
-            four_corner_codes_dict["bottom_left"][four_corner_code_str[2]].append(character)
-            four_corner_codes_dict["bottom_right"][four_corner_code_str[3]].append(character)
-            four_corner_codes_dict["extra"][four_corner_code_str[5]].append(character)
-
-    write_js_json(four_corner_codes_dict, "four_corner")
-
-    with open(generated_assets_dir + "four_corner.js", "a") as four_corner_js_output:
-        four_corner_js_output.write("\n")
-        four_corner_js_output.write("const FOUR_CORNER_INFO = ")
-        four_corner_js_output.write(open(static_assets_dir + "four_corner_info.json").read())
-
-def generate_radicals_file():
-    radicals_dict = {}
 
     radicals_info = json.loads(open(static_assets_dir + "radicals_info.json").read())
     for radical_info in radicals_info:
         radical_id = radical_info["radical_id"]
-        if radical_id not in radicals_dict.keys():
-            radicals_dict[radical_info["radical_id"]] = {
-                "kanji": [], "radical_characters": [
+        if radical_id not in radicals_info_dict.keys():
+            radicals_info_dict[radical_info["radical_id"]] = [
                     {
                         "character": radical_info["character"], "stroke_count": radical_info["stroke_count"]
                     }
                 ]
-            }
         else:
-            radicals_dict[radical_info["radical_id"]]["radical_characters"].append({"character": radical_info["character"], "stroke_count": radical_info["stroke_count"]})
+            radicals_info_dict[radical_info["radical_id"]].append({"character": radical_info["character"], "stroke_count": radical_info["stroke_count"]})
 
     kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
     for character_data in re.findall("<character>.*?</character>", kanjidic):
         character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)[0]
+        stroke_count = re.search(r"(?<=<stroke_count>)\d+(?=</stroke_count>)", character_data)
+        four_corner_code = re.search(r"(?<=<q_code qc_type=\"four_corner\">).*?(?=</q_code>)", character_data)
         radical = re.search(r"(?<=<rad_value rad_type=\"classical\">).*?(?=</rad_value>)", character_data)
+        skip_code = re.search(r"(?<=<q_code qc_type=\"skip\">).*?(?=</q_code>)", character_data)
+
+        if character in kanji_data:
+            print("dupe character found, this shouldnt happen")
+
+        kanji_data[character] = copy.deepcopy(default_kanji_data)
+
+        if stroke_count:
+            kanji_data[character]["stroke_count"] = int(stroke_count[0])
+            kanji_stroke_counts[character] = int(stroke_count[0])
+        else:
+            del kanji_data[character]["stroke_count"]
+
         if radical:
             radical_id = int(radical[0])
-            radicals_dict[radical_id]["kanji"].append(character)
+            kanji_data[character]["radical"]["id"] = radical_id
+            kanji_data[character]["radical"]["characters"] = radicals_info_dict[radical_id]
+        else:
+            del kanji_data[character]["radical"]
 
-    write_js_json(radicals_dict, "radicals")
+        if four_corner_code:
+            four_corner_code_str = four_corner_code[0]
+            kanji_data[character]["four_corner"]["top_left"] = four_corner_code_str[0]
+            kanji_data[character]["four_corner"]["top_right"] = four_corner_code_str[1]
+            kanji_data[character]["four_corner"]["bottom_left"] = four_corner_code_str[2]
+            kanji_data[character]["four_corner"]["bottom_right"] = four_corner_code_str[3]
+            kanji_data[character]["four_corner"]["extra"] = four_corner_code_str[5]
+        else:
+            del kanji_data[character]["four_corner"]
 
-def generate_skip_file():
-    skip_codes_dict = {
-        "part_one": {},
-        "part_two": {},
-        "part_three": {},
-    }
-
-    kanjidic = open(data_assets_dir + "kanjidic2.xml").read().replace("\n", "").replace("\r", "")
-    for character_data in re.findall("<character>.*?</character>", kanjidic):
-        character = re.search(r"(?<=<literal>).*?(?=</literal>)", character_data)[0]
-        skip_code = re.search(r"(?<=<q_code qc_type=\"skip\">).*?(?=</q_code>)", character_data)
         if skip_code:
             skip_code = list(map(int, skip_code[0].split("-"))) # remove random inconsistent zero padding
-            if (skip_code[0] in skip_codes_dict["part_one"].keys()):
-                skip_codes_dict["part_one"][skip_code[0]].append(character)
-            else:
-                skip_codes_dict["part_one"][skip_code[0]] = [character]
+            kanji_data[character]["skip"]["part_one"] = skip_code[0]
 
-            if (skip_code[1] in skip_codes_dict["part_two"].keys()):
-                skip_codes_dict["part_two"][skip_code[1]].append(character)
-            else:
-                skip_codes_dict["part_two"][skip_code[1]] = [character]
+            kanji_data[character]["skip"]["part_two"] = skip_code[1]
 
-            if (skip_code[2] in skip_codes_dict["part_three"].keys()):
-                skip_codes_dict["part_three"][skip_code[2]].append(character)
-            else:
-                skip_codes_dict["part_three"][skip_code[2]] = [character]
+            kanji_data[character]["skip"]["part_three"] = skip_code[2]
+        else:
+            del kanji_data[character]["skip"]
 
+    components_dict = generate_components_data()
+    for component, component_info in components_dict.items():
+        for component_kanji in component_info["kanji"]:
+            kanji_data[component_kanji]["components"].append(component)
 
-    write_js_json(skip_codes_dict, "skip")
+    for kanji in kanji_data:
+        if len(kanji_data[kanji]["components"]) == 0:
+            del kanji_data[kanji]["components"]
+
+    write_js_json(kanji_data, "kanji_data")
+
+def pack_info_files():
+    components_info = open(static_assets_dir + "components_info.json").read()
+    four_corner_info = open(static_assets_dir + "four_corner_info.json").read()
+    radicals_info = open(static_assets_dir + "radicals_info.json").read()
+
+    with open(generated_assets_dir + "packed_info.js", "w", encoding = "utf8") as packed_info:
+        packed_info.write("const COMPONENTS_INFO = " + components_info + "\n")
+        packed_info.write("const FOUR_CORNER_INFO = " + four_corner_info + "\n")
+        packed_info.write("const RADICALS_INFO = " + radicals_info + "\n")
+
 
 validation_regex = re.compile("(CJK (UNIFIED|COMPATIBILITY) IDEOGRAPH|HIRAGANA|IDEOGRAPHIC ITERATION MARK)")
 kanji_regex = re.compile("(CJK (UNIFIED|COMPATIBILITY) IDEOGRAPH)")
@@ -177,8 +190,6 @@ def generate_word_list():
 
     write_js_json(words, "words_list")
 
-generate_radicals_file()
-generate_components_file()
-generate_four_corner_file()
-generate_skip_file()
+parse_kanjidic_data()
+pack_info_files()
 generate_word_list()
